bed8da2589598d13ec4d054296d90eb7da84394c
leehaemin <2haemin@naver.com>
2024-01-31 15:23:22 +0900

    rename: gitignore에 properties 추가

--- backend/src/main/java/com/ktds/haru/api/user/presentation/controller/UserController.java
+++ backend/src/main/java/com/ktds/haru/api/user/presentation/controller/UserController.java
@@ -1,5 +1,35 @@
 package com.ktds.haru.api.user.presentation.controller;
 
+import com.ktds.haru.api.common.BaseResponse;
+import com.ktds.haru.api.user.presentation.dto.request.LoginRequestDTO;
+import com.ktds.haru.api.user.presentation.dto.request.UserRequestDTO;
+import com.ktds.haru.api.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
+
+    private final UserService userService;
+
+    /*
+    * 회원가입
+    * */
+    public BaseResponse<?> signUp(@RequestBody UserRequestDTO userRequestDTO){
+        return null;
+    }
+    
+    /*
+    * 로그인
+    * */
+    @PostMapping("/login")
+    public BaseResponse<?> login(@RequestBody LoginRequestDTO loginRequestDTO){
+        return null;
+    }
 
 }
